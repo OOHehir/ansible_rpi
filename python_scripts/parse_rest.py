98ee61913921016948e65ad938001cd48dedfdba
OOHehir <40183643+OOHehir@users.noreply.github.com>
2025-03-04 18:14:33 +0000

    Tidy up

--- python_scripts/parse_rest.py
+++ python_scripts/parse_rest.py
@@ -23,12 +23,12 @@ host = 'http://localhost:6345/'  # Local
 
 url =  'getCache'
 
-if (len(sys.argv) < 2):
-    print ("Please provide the key to search & optionally the test data file, e.g. ./parse_rest.py Battery_SOC")
-    sys.exit()
+if len(sys.argv) < 2:
+    print ("Provide the key to search & optionally the test data file, e.g. ./parse_rest.py Battery_SOC")
+    sys.exit(1)
 
-# Function to find the key in the nested dictionary
 def search_json(json_input, lookup_key):
+    '''Function to find the key in the nested dictionary'''
     if isinstance(json_input, dict):
         for k, v in json_input.items():
             if k == lookup_key:
@@ -36,35 +36,35 @@ def search_json(json_input, lookup_key):
             else:
                 yield from search_json(v, lookup_key)
     elif isinstance(json_input, list):
-        for item in json_input:
-            yield from search_json(item, lookup_key)
+        for item_l in json_input:
+            yield from search_json(item_l, lookup_key)
 
 # Try to find in following order:
 # 1. test data file
 # 2. URL
-if (len(sys.argv) > 2):
+if len(sys.argv) > 2:
     try:
-        f = open(sys.argv[2])
-        print ('Using test data file:', sys.argv[2])
-        data = json.load(f)
+        with open(sys.argv[2], encoding="utf-8") as f:
+            print ('Using test data file:', sys.argv[2])
+            data = json.load(f)
     except FileNotFoundError:
         print ('Failed to open file:', sys.argv[2])
 else:
     # Get the data from the REST API
     try:
-        response = requests.get(host + url)
+        response = requests.get(host + url, timeout=20)
         print ('Connecting to:', host + url)
     except requests.exceptions.RequestException as e:
         print ('Failed to connect to:', host + url)
-        exit(1)
+        sys.exit(1)
 
     data = response.json()
     # Find every occurrence of the key in the nested dictionary & create JSON
     # Format: {"key": value}
     for item in search_json(data, sys.argv[1]):
-        if type(item) == int:
+        if isinstance(item, int):
             print (start_data + "\"" + sys.argv[1] + "\"" + separate_data + str(item) + end_data)
         else:
             # Add quotes to the string
             print (start_data + "\"" + sys.argv[1] + "\"" + separate_data + "\"" + str(item) + "\"" + end_data)
-    exit(0)
+    sys.exit(0)
