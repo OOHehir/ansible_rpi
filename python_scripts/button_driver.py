d00576c93e6782494cd0e3426d6af49b1130a66f
OOHehir <40183643+OOHehir@users.noreply.github.com>
2025-03-04 18:07:48 +0000

    Move func to button_driver.py

--- python_scripts/button_driver.py
+++ python_scripts/button_driver.py
@@ -10,25 +10,35 @@ except RuntimeError:
 
 import time
 import os
+import subprocess
 import RPi.GPIO as GPIO
 
 time_pressed = 0
 BUTTON_GPIO = 22
 
-def button_event(time_pressed: float) -> None:
+def button_event(time_p: float) -> None:
     """
     Handle the button press event
     """
-    if time_pressed >5:
+    if time_p >5:
+        check_cmd = "sudo wpa_cli -i wlan0 status | grep -Fxq wpa_state=SCANNING"
+        status = subprocess.check_output(check_cmd, shell=True)
+        if status == 0:
+            print("WPS is in progress")
+            return
+
+        # Clear the network
+        clear_cmd = "sudo sed  -i '/network={/,$d' /etc/wpa_supplicant/wpa_supplicant-wlan0.conf"
+        os.system(clear_cmd)
+        time.sleep(5)
+
         # Start WPS
         print("Starting WPS")
-        cm = "sudo wpa_cli -i wlan0 wps_pbc"
-        os.system(cm)
-    if time_pressed > 3:
-        print("Button pressed for {} seconds".format(time_pressed))
+        start_cmd = "sudo wpa_cli -i wlan0 wps_pbc"
+        os.system(start_cmd)
 
-    time_pressed = 0
-
+    if time_p > 3:
+        print(f"Button pressed for {time_p} seconds")
 
 def rising_edge_callback():
     """
@@ -37,6 +47,7 @@ def rising_edge_callback():
     global time_pressed
     time_pressed = time.time() - time_pressed
     button_event(time_pressed)
+    time_pressed = 0
 
 def falling_edge_callback():
     """
@@ -55,5 +66,7 @@ if __name__ == '__main__':
     GPIO.add_event_detect(BUTTON_GPIO, GPIO.RISING, callback=rising_edge_callback(), bouncetime=100)
 
     while True:
+        time.sleep(1)
 
+    # Unreachable code
     GPIO.cleanup()
