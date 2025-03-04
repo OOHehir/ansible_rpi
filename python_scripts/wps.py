018706d8dd6ddf06f710a057bcf56d5d3f0d5942
OOHehir <40183643+OOHehir@users.noreply.github.com>
2025-03-04 17:59:43 +0000

    Improve script

--- python_scripts/wps.py
+++ python_scripts/wps.py
@@ -1,146 +1,45 @@
 #!/usr/bin/python
+""" Script to start WPS """
 import os
 import subprocess
 import time
+import sys
 
-class fake_fp:
-    def show(self, msg):
-        print('fake_fp: ' + msg)
+def clear_network():
+    """
+    Clear the network
+    """
+    clear_cmd = "sudo sed  -i '/network={/,$d' /etc/wpa_supplicant/wpa_supplicant-wlan0.conf"
+    os.system(clear_cmd)
 
-    def reset(self):
-        self.show('done')
+def start_wps():
+    """
+    Start WPS
+    """
+    start_cmd = "sudo wpa_cli -i wlan0 wps_pbc"
+    os.system(start_cmd)
 
-def display(msg=None, reset=False):
-    fp.show(msg)
-    time.sleep(2)
-    if reset:
-        fp.reset()
-
-def get_ssids(scan):
-    ssids = []
-    #print 'TEST:' + scan
-    try:
-        lines = scan.split('\n')
-        n = len(lines)
-        #print 'Num of lines: ' + str(n)
-        if n <= 2:
-            return -1
-        #print lines
-        i = 0
-        for l in lines:
-            #print 'line ' + str(i) + ' ' + repr(l)
-            _ = l.split()
-            if _ == []:
-                #print 'Empty line.'
-                pass
-            elif i > 1:
-                #print _
-                ssids.append(_[-1])
-            i += 1
-        #print 'num of ssids: ' + str(len(ssids))
-        #for ssid in ssids:
-        #    print ssid
-        return ssids
-    except Exception as e:
-        print('Failed get_ssids()')
-        return -1
-
-def shell_cmd(cmd):
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE,
-                              stderr=subprocess.PIPE)
-    out, err = p.communicate()
-    if err:
-        print (err)
-        return -1
+def check_wps_status():
+    """
+    Check the WPS status
+    """
+    check_cmd = "sudo wpa_cli -i wlan0 status | grep -Fxq wpa_state=SCANNING"
+    status = subprocess.check_output(check_cmd, shell=True)
+    if status == 0:
+        print("WPS is in progress")
+        return True
     else:
-        #print out
-        return out
+        print("WPS is not in progress")
+    return False
 
-def _scan():
-    cmd = ['wpa_cli', 'scan']
-    resp = shell_cmd(cmd)
-    if resp == -1:
-        return -1
-    else:
-        return 0
+if __name__ == '__main__':
+    # Check if WPS is in progress
+    if check_wps_status():
+        sys.exit(0)
 
-def scan():
-    # First scan
-    # Then get results
-    resp = _scan()
-    if resp == -1:
-        return -1
+    # Clear the network
+    clear_network()
+    time.sleep(5)
 
-    # Ok, get results
-    cmd = ['wpa_cli', 'scan_results']
-    resp = shell_cmd(cmd)
-    if resp == -1:
-        return -1
-    else:
-        #print resp
-        return resp
-
-def wps_connect(ssid):
-    cmd = ['wpa_cli', 'wps_pbc', ssid]
-    resp = shell_cmd(cmd)
-    if resp == -1:
-        return -1
-    else:
-        print (repr(resp))
-        return 0
-
-def halt():
-    if not is_fake_fp:
-        os.system('halt')
-
-def reboot():
-    if not is_fake_fp:
-        os.system('reboot')
-
-if __name__ == "__main__":
-    curr_dir = os.getcwd()
-    fname = 'scan_results.txt'
-    of = os.path.join(curr_dir, fname)
-    try:
-        import bap_com
-        fp = bap_com.FrontPanel()
-        is_fake_fp = False
-    except Exception as e:
-        print ('Using fake fp')
-        fp = fake_fp()
-        is_fake_fp = True
-
-    display('wps')
-    scan_res = scan()
-    if scan_res == -1:
-        display('scan error', True)
-        exit(1)
-
-    ssids = get_ssids(scan_res)
-    if ssids == -1:
-        display('ssid error', True)
-        exit(1)
-
-    i = 1
-    with open(of, 'w') as f:
-        for ssid in ssids:
-            f.write(ssid + '\n')
-            display(ssid + ' - ' + str(i))
-            i+=1
-
-    resp = 0
-    magic_ssid = 'dont check' #'bohmeraudio'
-    if magic_ssid in ssids:
-        display('Connecting...')
-        resp = wps_connect(magic_ssid)
-        if resp == -1:
-            display('Failed.')
-        else:
-            display('Connected')
-            display('Restarting')
-            reboot()
-
-    display('Finished')
-    display('Start AP')
-    time.sleep(1)
-    halt()
+    # Start
+    start_wps()
