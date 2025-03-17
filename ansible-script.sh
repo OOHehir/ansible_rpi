d1a89bbd5d6cb047095806ad2f1247bef6424e42
OOHehir <40183643+OOHehir@users.noreply.github.com>
2025-03-17 21:38:41 +0000

    Use pip to install ansible

--- ansible-script.sh
+++ ansible-script.sh
@@ -4,25 +4,32 @@ ANSIBLE_PATH=/home/octopus/ansible
 ANSIBLE_CONTROL_NODE_URL='https://github.com/OOHehir/ansible_rpi.git'
 
 first_boot_setup () {
+    # Apt update and install git
     apt update
-    apt install ansible-core git apache2 -y
+    apt install git python3-pip -y
+
+    # Install pip and ansible
+    python3 -m pip install --user ansible-core
 
     # Clone the ansible repository
-    cd $ANSIBLE_PATH
-    git clone $ANSIBLE_CONTROL_NODE_URL
+    # cd $ANSIBLE_PATH
+    # git clone $ANSIBLE_CONTROL_NODE_URL
 
     # If successful, create a file to indicate that the first boot setup has been completed
+    mkdir -p $ANSIBLE_PATH
     echo $(date) > $ANSIBLE_PATH/ansible-first-boot.log
 }
 
 update_and_run_ansible () {
     # Pull latest changes from the git repository
-    cd $ANSIBLE_PATH/ansible_rpi
+    # cd $ANSIBLE_PATH/ansible_rpi
     # Probably need to change this to a curl from a server
-    git pull
+    # git pull
 
     # A little artificial but use eventually use this to update & run
-    ansible-pull -U $ANSIBLE_PATH/ansible_rpi -C main playbook.yml
+    #/usr/bin/ansible-pull -U https://github.com/OOHehir/ansible_rpi.git -d /home/octopus/ansible --diff playbook.yml
+    /usr/bin/ansible-pull -U https://github.com/OOHehir/ansible_rpi.git -d /home/octopus/ansible --diff playbook.yml
+    # ansible-pull -U $ANSIBLE_PATH/ansible_rpi -C main playbook.yml
 
     # log the operation, continue if file doesn't exist
     rm $ANSIBLE_PATH/ansible.log || true
